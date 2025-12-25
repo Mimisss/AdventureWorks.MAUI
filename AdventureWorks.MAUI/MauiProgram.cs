88f75e1ed6da651d548d9aca980fb2d1d7ac7659
Mimisss <pantazop.dim@gmail.com>
2025-12-25 19:21:12 +0200

    Make the Window Maximized (on Windows)

--- AdventureWorks.MAUI/MauiProgram.cs
+++ AdventureWorks.MAUI/MauiProgram.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿#if WINDOWS
+using Microsoft.Maui.LifecycleEvents;
+using WinRT.Interop;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+#endif
+using Microsoft.Extensions.Logging;
 
 namespace AdventureWorks.MAUI
 {
@@ -7,6 +13,7 @@ namespace AdventureWorks.MAUI
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
@@ -15,11 +22,47 @@ namespace AdventureWorks.MAUI
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+#if WINDOWS
+            SetWindowOptions(builder);
+#endif
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
         }
+
+
+#if WINDOWS
+        // Make the Window Maximized (only applicable on Windows)
+        public static void SetWindowOptions(MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(windows =>
+                {
+                    windows.OnWindowCreated(window =>
+                    {
+                        IntPtr windowHandle = WindowNative.GetWindowHandle(window);
+
+                        WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+
+                        AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+                        if (appWindow.Presenter is OverlappedPresenter p)
+                        {
+                            p.Maximize();
+
+                            //p.IsAlwaysOnTop = false;
+                            //p.IsMaximizable = false;
+                            //p.IsMinimizable = false;
+                            //p.IsResizable = false;
+                        }
+                    });
+                });
+            });
+        }
+#endif
     }
 }
