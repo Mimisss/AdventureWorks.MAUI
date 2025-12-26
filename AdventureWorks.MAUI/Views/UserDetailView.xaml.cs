a22cf965ae3688fcc33ef7ee6dce3b71fe0c7fee
Mimisss <pantazop.dim@gmail.com>
2025-12-26 21:59:49 +0200

    Create Several Pages

---
+++ AdventureWorks.MAUI/Views/UserDetailView.xaml.cs
@@ -0,0 +1,14 @@
+namespace AdventureWorks.MAUI.Views;
+
+public partial class UserDetailView : ContentPage
+{
+	public UserDetailView()
+	{
+		InitializeComponent();
+	}
+
+	private void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		// TODO: Respond to the event here		
+    }
+}
