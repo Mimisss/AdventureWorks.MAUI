41bf602d5549916ecac2e9ca22558af9a2aab167
Mimisss <pantazop.dim@gmail.com>
2026-01-02 21:57:14 +0200

    Use a ValueConverter to Change a Boolean Value

---
+++ Common.Library.MAUI/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Common.Library.MAUI.Converters
+{
+    public class InvertedBoolConverter : IValueConverter
+    {        
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+
+            return value;
+        }
+
+        // Convert the visual representation of the data to the specific data type
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
