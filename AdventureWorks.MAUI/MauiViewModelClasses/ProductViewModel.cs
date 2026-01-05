1848e9965edb8ed58a8ff9e47e8e20b2b45b818d
Mimisss <pantazop.dim@gmail.com>
2026-01-05 16:39:10 +0200

    Create a Product View Model for Commanding

---
+++ AdventureWorks.MAUI/MauiViewModelClasses/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AdventureWorks.EntityLayer;
+using Common.Library;
+
+namespace AdventureWorks.MAUI.MauiViewModelClasses
+{
+    public class ProductViewModel : ViewModelLayer.ProductViewModel
+    {
+        public ProductViewModel() : base()
+        {
+        }
+
+        public ProductViewModel(IRepository<Product> repo) : base(repo)
+        {
+        }
+    }
+}
