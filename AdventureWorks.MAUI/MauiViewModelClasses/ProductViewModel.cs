def647a6b2e34eb4c31e28049ef0edabe49d111f
Mimisss <pantazop.dim@gmail.com>
2026-01-05 17:25:04 +0200

    Display Product Detail from Collection View

--- AdventureWorks.MAUI/MauiViewModelClasses/ProductViewModel.cs
+++ AdventureWorks.MAUI/MauiViewModelClasses/ProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using AdventureWorks.EntityLayer;
 using Common.Library;
+using System.Windows.Input;
 
 namespace AdventureWorks.MAUI.MauiViewModelClasses
 {
@@ -11,6 +12,45 @@ namespace AdventureWorks.MAUI.MauiViewModelClasses
 
         public ProductViewModel(IRepository<Product> repo) : base(repo)
         {
+        }
+
+        public ICommand? SaveCommand { get; private set; }
+
+        public ICommand? EditCommand { get; private set; }
+
+        public ICommand? CancelCommand { get; private set; }
+
+        public override void Init()
+        {
+            base.Init();
+
+            SaveCommand = new Command(async () => await SaveAsync());
+
+            EditCommand = new Command<int>(async (int id) => await EditAsync(id));
+
+            CancelCommand = new Command(async () => await CancelAsync());
+        }
+
+        public async Task EditAsync(int id)
+        {
+            await Shell.Current.GoToAsync($"{nameof(Views.ProductDetailView)}?id={id}");
+        }
+
+        public override async Task<Product?> SaveAsync()
+        {
+            Product? result = await base.SaveAsync();
+
+            if (result != null)
+            {
+                await Shell.Current.GoToAsync("..");
+            }
+
+            return result;
+        }
+
+        public async Task CancelAsync()
+        {
+            await Shell.Current.GoToAsync("..");
         }
     }
 }
--- AdventureWorks.MAUI/Views/ProductDetailView.xaml.cs
+++ AdventureWorks.MAUI/Views/ProductDetailView.xaml.cs
@@ -1,11 +1,31 @@
+using AdventureWorks.MAUI.MauiViewModelClasses;
+
 namespace AdventureWorks.MAUI.Views;
 
+[QueryProperty(nameof(ProductId), "id")]
 public partial class ProductDetailView : ContentPage
 {
-	public ProductDetailView()
+	private readonly ProductViewModel viewModel;
+
+    public ProductDetailView(ProductViewModel viewModel)
 	{
 		InitializeComponent();
+
+		this.viewModel = viewModel;
 	}
+
+	public int ProductId { get; set; }
+
+	protected async override void OnAppearing()
+	{
+		base.OnAppearing();
+
+        // Set the BindingContext to the ViewModel
+        BindingContext = viewModel;
+
+        // Retrieve a Product
+        await viewModel.GetAsync(ProductId);
+    }
 
     private void Weight_Changed(object sender, ValueChangedEventArgs e)
     {
